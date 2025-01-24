28f04639b3f055e64b679cc4c216727f365c278f
Girobusan <private>
2025-01-24 09:55:11 +0300

    data fetch improvement

--- src/data_fetch.js
+++ src/data_fetch.js
@@ -1,4 +1,16 @@
 if (!fetch.isAlreadyFixed) {
+  function traceData(path, data) {
+    if (!data) {
+      data = window.impData;
+    }
+    const PA = path.split("/");
+    return PA.reduce((a, e) => {
+      if (!a) {
+        return a;
+      }
+      return a[e];
+    }, data);
+  }
   function tryParse(d) {
     let r = d;
     try {
@@ -14,11 +26,12 @@ if (!fetch.isAlreadyFixed) {
   window.fetch = function(url, opts) {
     if (url.startsWith("@")) {
       const dataId = url.substring(1);
-      if (!window.impData[dataId]) {
+      const data = traceData(dataId, window.impData);
+      if (!data) {
         return Promise.reject("No such data");
       }
-      const dataType = typeof window.impData[dataId];
-      const d = window.impData[dataId];
+      const dataType = typeof data;
+      const d = data;
       return Promise.resolve({
         ok: true,
         status: 200,
